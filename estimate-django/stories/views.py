49456435c79758ed7a18e3c103795fc9d2149c67
ruyadorno <ruyadorno@hotmail.com>
2012-10-05 16:04:19 -0300

    * Implemented bootstrap visual

--- estimate-django/stories/views.py
+++ estimate-django/stories/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 from stories.models import Project
 
 
 def index(request):
-    return render_to_response('index.html', {'projects':Project.objects.all()})
+    context = RequestContext(request, {'projects':Project.objects.all()})
+    return render_to_response('index.html', context)
