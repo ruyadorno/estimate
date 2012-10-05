1ff587b512b9d925b77d54759d999f7c6251bad0
ruyadorno <ruyadorno@hotmail.com>
2012-10-05 11:42:51 -0300

    * Adding index

--- estimate-django/estimate/urls.py
+++ estimate-django/estimate/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'estimate.views.home', name='home'),
+    url(r'^$', 'estimate.views.home', name='home'),
     # url(r'^estimate/', include('estimate.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
---
+++ estimate-django/estimate/views.py
@@ -0,0 +1,3 @@
+from django.shortcuts import render_to_response
+def home(request):
+    return render_to_response('base.html', {})
