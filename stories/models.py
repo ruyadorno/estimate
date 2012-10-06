db03c6c137f1b2550618818ec2256f8171c90b3d
ruyadorno <ruyadorno@hotmail.com>
2012-10-06 13:03:24 -0300

    Adding Story model

--- stories/models.py
+++ stories/models.py
@@ -3,4 +3,10 @@ from django.db import models
 
 class Project(models.Model):
     name = models.CharField(max_length=100)
+    description = models.TextField()
+    active = models.BooleanField()
+
+class Story(models.Model):
+    name = models.CharField(max_length=100)
+    time = models.IntegerField()
     accepted = models.BooleanField()
