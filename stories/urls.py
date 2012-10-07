416367cf4688cacbbab67a569db3f2a4ba260284
ruyadorno <ruyadorno@hotmail.com>
2012-10-07 19:21:17 -0300

    Added story time change Changed a bunch of urls around

--- stories/urls.py
+++ stories/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     url(r'^$', 'stories.views.index', name='stories_index'),
-    url(r'^add/', 'stories.views.add', name='stories_add'),
-    url(r'^remove/', 'stories.views.remove', name='stories_remove'),
-    url(r'^project/(?P<project_id>\d+)/$', 'stories.views.project', name='project'),
+    url(r'^add/', 'stories.views.add_project', name='stories_add_project'),
+    url(r'^remove/', 'stories.views.remove_project', name='stories_remove_project'),
+    url(r'^project/(?P<project_id>\d+)/$', 'stories.views.project_page', name='project_page'),
+    url(r'^change_story_time/$', 'stories.views.change_story_time', name='change_story_time'),
 )
--- stories/views.py
+++ stories/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from django.shortcuts import redirect, render_to_response, get_object_or_404, get_list_or_404
+from django.shortcuts import redirect, render_to_response, get_object_or_404
 from django.template import RequestContext
 
 from stories.models import Project, Story
@@ -10,7 +10,7 @@ def index(request):
     context = RequestContext(request, {'projects':Project.objects.all(), 'form':ProjectForm()})
     return render_to_response('index.html', context)
 
-def add(request):
+def add_project(request):
     if request.method == 'POST':
         project = Project(active=True)
         form = ProjectForm(request.POST, instance=project)
@@ -18,13 +18,13 @@ def add(request):
             form.save()
             return redirect('stories_index')
         else:
-            return add_error(request, form)
+            return add_project_error(request, form)
 
-def add_error(request, form):
+def add_project_error(request, form):
     context = RequestContext(request, {'projects':Project.objects.all(), 'form':form})
     return render_to_response('index.html', context)
 
-def remove(request):
+def remove_project(request):
     if request.method == 'POST':
         try:
             delete_id = request.POST['delete_id']
@@ -36,9 +36,30 @@ def remove(request):
     else:
         raise Http404
 
-def project(request, project_id):
+def project_page(request, project_id):
     project = get_object_or_404(Project, id=project_id)
-    stories = Story.objects.all()
-    #get_list_or_404(Story, )
+    stories = Story.objects.filter(project_id=project.id)
     context = RequestContext(request, {'project':project, 'stories':stories})
     return render_to_response('project.html', context)
+
+def change_story_time(request):
+    if request.method == 'POST':
+        try:
+            story_id = request.POST['id']
+        except KeyError:
+            return redirect('stories_index')
+        story = get_object_or_404(Story, id=story_id)
+        try:
+            time = request.POST['time']
+        except KeyError:
+            return redirect('project_page', project_id=story.project_id)
+        story.time = time
+        try:
+            story.save()
+        except Exception:
+            #TODO: Return to the project page adding a custom error message
+            #Actually, that would be a nice behaviour for add/remove actions
+            pass
+        return redirect('project_page', project_id=story.project_id)
+    else:
+        raise Http404
