e14147f2890a1bc7dc1dc693305e2568cd61f48c
ruyadorno <ruyadorno@hotmail.com>
2012-10-07 22:20:13 -0300

    Adding total time for project

--- stories/models.py
+++ stories/models.py
@@ -6,6 +6,17 @@ class Project(models.Model):
     description = models.TextField()
     active = models.BooleanField()
 
+    def _get_total_time(self):
+        try:
+            stories = Story.objects.filter(project_id=self.id)
+        except Exception:
+            return 0
+        times = [story.time for story in stories]
+        return sum(times)
+
+    total_time = property(_get_total_time)
+
+
 class Story(models.Model):
     name = models.CharField(max_length=100)
     time = models.IntegerField()
