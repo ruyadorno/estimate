f64531e8049211b9a1889cbc5cc9e84c4440cf53
ruyadorno <ruyadorno@hotmail.com>
2012-10-28 23:30:42 -0200

    * Adding first test cases

--- stories/tests.py
+++ stories/tests.py
@@ -1,14 +1,29 @@
-from django.utils import unittest
 from django.test import TestCase
+from django.test.client import Client
 
 from stories.models import Project, Story
 
 
 class SimpleTest(TestCase):
+
     fixtures = ['test.json',]
-    def test_basic_addition(self):
-        print Project.objects.all()
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+
+    def setUp(self):
+        "Set up test data"
+        self.client = Client()
+        self.projects = Project.objects.all()
+        self.active_projects = Project.objects.filter(active=True)
+
+    def test_fixtures(self):
+        "Test fixtures are loaded and data is accessible"
+        self.assertNotEqual(self.projects.count(), 0)
+
+    def test_index_page(self):
+        "Test the loading of index page"
+        response = self.client.get('/stories/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            len(response.context['projects']), self.active_projects.count()
+        )
+        for project in self.active_projects:
+            self.assertContains(response, project.name, 1)
